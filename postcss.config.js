0166fc983852228ad23d692bb3000d29fbf6706e
Patrick Kollitsch <patrick@davids-neighbour.com>
2019-08-16 21:12:29 +0700

    fix: multiple fixes to theme files

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,8 @@
 module.exports = {
-	plugins: {
-		autoprefixer: {
-			browsers: [
-				">= 1%",
-				"last 2 major versions",
-				"not dead",
-				"Chrome >= 45",
-				"Firefox >= 38",
-				"Edge >= 12",
-				"Explorer >= 10",
-				"iOS >= 9",
-				"Safari >= 9",
-				"Android >= 4.4",
-				"Opera >= 30"
-			]
-		}
-	},
+    plugins: {
+        autoprefixer: {},
+        cssnano:{
+            preset: 'default',
+        },
+    },
 }
