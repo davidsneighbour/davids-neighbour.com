05cd706b4eacc0ab25623892be297b0e1d14bb5f
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-07-18 17:08:36 +0700

    feat: add tailwind, remove bootstrap

--- postcss.config.js
+++ postcss.config.js
@@ -16,3 +16,14 @@ module.exports = {
         //...(process.env.HUGO_ENVIRONMENT === 'production' ? [ purgecss ] : [])
     }
 }
+let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
+const tailwind = require('tailwindcss')(tailwindConfig);
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  // eslint-disable-next-line no-process-env
+  plugins: [
+    tailwind,
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ autoprefixer ] : [])
+  ]
+};
