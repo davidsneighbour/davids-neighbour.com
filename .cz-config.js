2b764c70c739058e0a91e075f4a098f4e306436a
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-08-15 12:33:47 +0700

    chore(config): formatting cz-config file

--- .cz-config.js
+++ .cz-config.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // @see https://github.com/leonardoanalista/cz-customizable
 module.exports = {
   types: [
@@ -68,8 +66,7 @@ module.exports = {
     subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
     breaking: "List any BREAKING CHANGES (optional):\n",
-    footer:
-      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
+    footer: "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
 
