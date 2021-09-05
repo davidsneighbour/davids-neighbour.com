24052302b85ec357b249dbd0b7b37ba2fab42139
Patrick Kollitsch <patrick@davids-neighbour.com>
2021-09-05 09:02:46 +0700

    feat(shortcode): add youtube lite shortcode as module

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,10 @@
 import Alpine from "alpinejs";
+import LiteYTEmbed from "lite-youtube-embed";
 
 // eslint-disable-next-line no-undef
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Register custom element
+customElements.define("lite-youtube", LiteYTEmbed);
--- postcss.config.js
+++ postcss.config.js
@@ -7,8 +7,8 @@ module.exports = {
     ...(process.env.HUGO_ENVIRONMENT === "production"
       ? { autoprefixer: {} }
       : {}),
-    "postcss-preset-env": {
-      features: { "nesting-rules": false },
-    },
+    // "postcss-preset-env": {
+    //   features: { "nesting-rules": false },
+    // },
   },
 };
