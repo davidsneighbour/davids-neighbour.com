1adde6925e9b679a427a2805af93d41fab2d8be8
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-21 05:42:11 +0700

    chore: cleanup and fixing astro setup
    Signed-off-by: Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  })],
-  srcDir: 'src',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx()]
 });
