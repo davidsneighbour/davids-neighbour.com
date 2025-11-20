c7c75ab192a53582649f536c02ec380301bccf9a
Patrick Kollitsch <83281+davidsneighbour@users.noreply.github.com>
2025-11-21 05:30:36 +0700

    refactor: migrate site to astro (#366)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx,md,mdx}'],
   theme: {
     fontSize: {
       xs: '0.75rem',
